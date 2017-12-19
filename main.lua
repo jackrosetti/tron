bb263e025751b265e9c95a4e0c94704e7c162830
Jack Rosetti <32694046+jackrosetti@users.noreply.github.com>
2017-12-18 18:11:51 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -38,37 +38,20 @@ function updateBikes()
 	end
 end
 
-local timerMax = .11
-local timer = timerMax
+timerMax = .1
+timer = timerMax
+score = 1
 
-function love.update(dt)
-	if timer > 0 then
-		timer = timer - dt
-	else
-		timer = timerMax
-		player1.trail[#player1.trail+1] = {x = player1.x, y = player1.y}
-		player2.trail[#player2.trail+1] = {x = player2.x, y = player2.y}
-		updateBikes()
-	end
 	if player2.x == player1.x and player2.y == player1.y then
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.print("Game over!", 0, 0)
-		love.timer.sleep(1)
-		love.event.quit("restart")
+		love.event.quit('restart')
 	end
-	
+
 	for i = 1, #player2.trail do
 		local j = player2.trail[i]
 		if j.x == player1.x and j.y == player1.y then
-			love.graphics.setColor(255, 255, 255)
-			love.graphics.print("Game over!", 0, 0)
-			love.timer.sleep(1)
 			love.event.quit('restart')
 		end
 		if j.x == player2.x and j.y == player2.y then
-			love.graphics.setColor(255, 255, 255)
-			love.graphics.print("Game over!", 0, 0)
-			love.timer.sleep(1)
 			love.event.quit('restart')
 		end
 	end
@@ -76,31 +59,39 @@ function love.update(dt)
 	for i = 1, #player1.trail do
 		local p = player1.trail[i]
 		if p.x == player2.x and p.y == player2.y then
-			love.graphics.setColor(255, 255, 255)
-			love.graphics.print("Game over!", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-			love.timer.sleep(1)
 			love.event.quit('restart')
 		end
 		if p.x == player1.x and p.y == player1.y then
-			love.graphics.setColor(255, 255, 255)
-			love.graphics.print("Game over!", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-			love.timer.sleep(1)
-			love.event.quit('restart') 
+			love.event.quit('restart')
 		end
 	end
 
 	if player1.x < 0 or player2.x < 0 or player2.y < 0 or player1.y < 0 then
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.print("Game over!", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-		love.timer.sleep(1)
-		love.event.quit("restart")
+		love.event.quit('restart')
 	end
 	if player2.y > heightRange or player1.y > heightRange or player1.x > widthRange or player2.x > widthRange then
-		love.graphics.setColor(255, 255, 255)
-		love.graphics.print("Game over!", love.graphics.getWidth()/2, love.graphics.getHeight()/2)
-		love.timer.sleep(1)
-		love.event.quit("restart")	end
-end
+		love.event.quit('restart')
+	end
+
+	print(#player1.trail)
+
+	--for j = 0, 250, 1 do
+	--	score = score + 1
+	--end
+
+	if #player1.trail > 10 then
+		timerMax = .09
+	end
+
+	if #player1.trail > 10 and #player1.trail < 40 then
+		timerMax = 0.01
+	end
+
+	if #player1.trail > 40 then
+		timerMax = .009
+	end
+
+--end
 
 function love.draw()
 	love.graphics.setColor(0, 255, 0)
@@ -109,21 +100,12 @@ function love.draw()
 		local b = player1.trail[i]
 		love.graphics.rectangle("line", b.x * tilesize, b.y * tilesize, tilesize, tilesize)
 	end
-	
+
 	love.graphics.setColor(255, 0, 0)
 	for i = 1, #player2.trail do
 		local c = player2.trail[i]
 		love.graphics.rectangle("line", c.x * tilesize, c.y * tilesize, tilesize, tilesize)
 	end
-	
-	love.graphics.rectangle("fill", player2.x *tilesize, player2.y * tilesize, tilesize, tilesize)
-end
 
-function love.keypressed(key)
-	if key == "right" or key == "left" or key == "down" or key == "up" then
-		player1.dir = key
-	end
-	if key == "w" or key =="a" or key == "s" or key =="d" then
-		player2.dir = key
-	end
+	love.graphics.rectangle("fill", player2.x *tilesize, player2.y * tilesize, tilesize, tilesize)
 end
